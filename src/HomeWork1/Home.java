e7255ec09c221fe917578039481514cd1b8a6be7
Yulia Shibanova <yuliashi@hotmail.com>
2022-03-07 21:58:37 -0500

    First homework2

---
+++ src/HomeWork1/Home.java
@@ -0,0 +1,28 @@
+package HomeWork1;
+
+public class Home {
+    public static void main(String[] args) {
+
+        byte hoursInaDay = 24;
+        System.out.println("Number of hours in a day-> " + hoursInaDay);
+        short daysInaYear = 365;
+        System.out.println("Number of days in a year -> " + daysInaYear);
+        int totalEmployee = 2500;
+        System.out.println("Number of Employees -> " + totalEmployee);
+        long populationEarth = 7900000000L;
+        System.out.println("Population of the Earth is " + populationEarth);
+        double mortgageRate = 2.5;
+        System.out.println("Average Mortgage Rate -> " + mortgageRate);
+        double accountBalance = 23850.33;
+        System.out.println("Your Account Balance is " + accountBalance);
+        boolean sunRise = true;
+        System.out.println("Does the sun rise on the West? " + sunRise);
+        char initialFirst = 'Y';
+        char initialSecond = 'S';
+        System.out.println("My Initials " + initialFirst + initialSecond);
+        String myName = "Yulia Shibanova";
+        System.out.println("My name is " + myName);
+
+
+    }
+}
